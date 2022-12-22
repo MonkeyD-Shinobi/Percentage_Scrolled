30dcbc0e37cbd1ee96a3a75968fae800a7d1c318
MonkeyD-Shinobi <108976808+MonkeyD-Shinobi@users.noreply.github.com>
2022-12-22 23:00:03 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+let scrollPercentage = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+
+  // scrollProgress.style.background = `conic-gradient(#008fff ${scrollValue}%, #c0c0ff ${scrollValue}%)`;
+  progressValue.textContent = `${scrollValue}% Viewed`;
+};
+
+window.onscroll = scrollPercentage;
+window.onload = scrollPercentage;
